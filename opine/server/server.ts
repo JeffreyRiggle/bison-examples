e73d8750b8917584ea22a182963102467f0edeef
Jeff <JeffreyRiggle@gmail.com>
2020-06-01 06:49:13 -0400

    minor changes to opine

--- opine/server/server.ts
+++ opine/server/server.ts
@@ -1,9 +1,10 @@
 import opine from 'https://deno.land/x/opine@master/mod.ts'
+import * as bison from 'https://unpkg.com/@jeffriggle/bison@1.0.4/dist/esm/index.js'
 
 const app = opine();
 
 app.get('/', (req, res) => {
-    res.send('Sup')
+    res.send(bison.encode({message: 'hello world'}))
 });
 
 app.listen(4040)
