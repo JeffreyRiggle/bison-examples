2655b1ed883a68be00eec79d7c90b133e4c50ddf
Jeff <JeffreyRiggle@gmail.com>
2020-06-15 20:44:14 -0400

    actually get opine working

--- opine/server/server.ts
+++ opine/server/server.ts
@@ -1,5 +1,5 @@
 import opine from 'https://deno.land/x/opine@master/mod.ts'
-import * as bison from 'https://unpkg.com/@jeffriggle/bison@1.0.4/dist/esm/index.js'
+import bison from 'https://unpkg.com/@jeffriggle/bison@1.0.8/dist/esm/index.js'
 
 const app = opine();
 
